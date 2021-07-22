277b143fd0ef5c10bef80bb12dc86d328ec7bb23
Kthewarlord <i.amken123@hotmail.com>
2021-07-22 23:18:19 +0700

    add manual hangup button

--- pages/videocall.vue
+++ pages/videocall.vue
@@ -17,11 +17,14 @@
           </div>
         </div>
     </div>
-    <div class='max-w-4xl mb-10 w-full h-auto justify-center bg-white shadow-xl rounded-lg overflow-hidden mx-auto flex flex-col px-5 pb-5 space-y-5'>
-        <div class = 'pt-10 px-5 pb-5 m-auto content-center'>
+    <div class='max-w-4xl mb-10 w-full h-auto justify-center bg-white shadow-xl rounded-lg overflow-hidden mx-auto flex flex-col px-4 pb-5 space-y-5'>
+        <div class = 'pt-10 px-4 pb-5 m-auto content-center'>
           <button @click='openRoom()' :style="{display : buttondisplay}" class="px-2 py-3 w-full m-auto bg-red-700 rounded-xl text-white shadow-lg hover:bg-red-400 hover:text-white">Join Here!</button>
           <div :style="{display : videodisplay}">
-              <div id="meet"></div>
+              <div class = "block">
+                <div id="meet"></div>
+                <button @click='leaveRoom()' :style="{display : videodisplay}" class="px-2 py-3 mt-5 mb-3 w-full m-auto bg-red-700 rounded-xl text-white shadow-lg hover:bg-red-400 hover:text-white">Leave Meeting</button>
+              </div>
           </div>
         </div>
     </div>
@@ -90,17 +93,56 @@ export default {
         const domain = "meet.jit.si";
         const options = {
           roomName: this.room,
-          width: 700,
-          height: 700,
+          width: '48em',
+          height: '48em',
           parentNode: document.querySelector("#meet"),
           interfaceConfigOverwrite: {
             filmStripOnly: false,
             SHOW_JITSI_WATERMARK: false,
+            SHOW_WATERMARK_FOR_GUESTS: false,
+            DEFAULT_REMOTE_DISPLAY_NAME: 'New User',
+            MOBILE_APP_PROMO: false,
+
+            SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+            //SHOW_BRAND_WATERMARK: true,
+
+            //JITSI_WATERMARK_LINK: '',
+            //Jitsi watermark destination when clicked
+
+            SHOW_CHROME_EXTENSION_BANNER: false,
+            //if set to true, it will show chrome extension banner message on top-right
+
+            //SHOW_POWERED_BY: true,
+            //SHOW_PPOWERED_BY will show the text powered by jitsi.org on bottom left
+
+            //HIDE_DEEP_LINKING_LOGO: false,
+            //JITSI_WATERMARK_LINK: ''
+             /*
+             * DEPRECATED!
+             * This config might be moved to config.js as `toolbarButtons`.
+             */
+            // TOOLBAR_BUTTONS: [
+            //     'microphone', 'camera', 'closedcaptions', 'desktop', 'embedmeeting', 'fullscreen',
+            //     'fodeviceselection', 'hangup', 'profile', 'chat', 'recording',
+            //     'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand',
+            //     'videoquality', 'filmstrip', 'invite', 'feedback', 'stats', 'shortcuts',
+            //     'tileview', 'select-background', 'download', 'help', 'mute-everyone', 'mute-video-everyone', 'security'
+            // ],
+            TOOLBAR_BUTTONS: ['microphone', 'camera', 'closedcaptions', 'profile', 'fullscreen','chat', 'settings',
+                              'raisehand','invite','recording','security','tileview','select-background','videoquality',
+                              'filmstrip','etherpad','stats']
+            // 'hangup' is the way to leave the meeting, include it in toolbar buttons if you don't want to use your own html buttons
+            //TOOLBAR_BUTTONS: []
+            //if left empty, will diasble the toolbar(make it disappear)
           },
           configOverwrite: {
             doNotStoreRoom: true,
             disableSimulcast: false,
-            startWithAudioMuted: true
+            startWithAudioMuted: true,
+            //enableWelcomePage: true
+            //prejoinPageEnabled: false,
+            disableRemoteMute: true,
+            //SHOW_JITSI_WATERMARK: false
           },
         };
 
@@ -128,6 +170,18 @@ export default {
         alert("Jitsi Meet API script not loaded");
       }
     },
+    leaveRoom() {
+      this.api.executeCommand('hangup',this.room);
+
+      this.$store.commit('setVideoDisplay',"none")
+      this.$store.commit('setButtonDisplay',"block")
+
+      //because iframe ads still stays after meeting ended, we go back to homepage
+      
+      //Link to first page
+      this.$router.push('/');
+
+    }
   },
 };
 </script>
